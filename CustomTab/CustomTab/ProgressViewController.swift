f8f2a4aad8c5ef77f788d5ccfa4e91264df9ee81
Benny Dalby <d4dalby@gmail.com>
2019-10-23 20:13:37 -0700

    Custom Tab Bar code with + icon in the middle

---
+++ CustomTab/CustomTab/ProgressViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ProgressViewController.swift
+//  CustomTab
+//
+//  Created by mac on 10/23/19.
+//  Copyright © 2019 mac. All rights reserved.
+//
+
+import UIKit
+
+class ProgressViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        view.backgroundColor = UIColor.white
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationItem.title = "Progress"
+        
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ CustomTab/CustomTab/SelfMonitorViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SelfMonitorViewController.swift
+//  CustomTab
+//
+//  Created by mac on 10/23/19.
+//  Copyright © 2019 mac. All rights reserved.
+//
+
+import UIKit
+
+class SelfMonitorViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = UIColor.white
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationItem.title = "Self Monitor"
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+}
